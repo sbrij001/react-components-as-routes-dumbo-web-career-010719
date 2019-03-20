59968a97482e9f55ded97cc640e67581b88fd22b
github username <github email address>
2019-03-20 17:42:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -7,9 +8,39 @@ const Home = () => {
       <h1>Home!</h1>
     </div>
   );
+
+const About = () => {
+  return(
+    <div>
+    <h1>This is my about component!</h1>
+    <div/>
+  )
 };
 
-ReactDOM.render(
-  <Home />,
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" render={Home}/>
+    <Route exact path="/about" render={About} />
+    <Route exact path="/login" render={Login} />
+  </Router>),
   document.getElementById('root')
 );
+};
